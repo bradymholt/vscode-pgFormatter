40b7b2951803c8801dfc46ed17e118569a7396ae
Brady Holt <brady.holt@gmail.com>
2018-01-15 17:24:52 -0600

    Add unit tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,22 +1,59 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
+import * as assert from "assert";
+import { workspace } from "vscode";
+import * as extension from "../extension";
 
-// The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-import * as myExtension from '../extension';
-
-// Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", () => {
+  test("Formatting SQL", () => {
+    let output = extension.getFormattedText(
+      "select id, first_name from people",
+      <extension.WorkspaceConfiguration>{},
+      <extension.FormattingOptions>{}
+    );
+    assert.equal(
+      output,
+      `\
+SELECT
+    id,
+    first_name
+FROM
+    people
+`
+    );
+  });
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
-    });
+  test("size", () => {
+    let output = extension.getFormattedText(
+      "select id, first_name from people",
+      <any>{ spaces: 3 },
+      <extension.FormattingOptions>{ tabSize: 8 }
+    );
+    assert.equal(
+      output,
+      `\
+SELECT
+   id,
+   first_name
+FROM
+   people
+`
+    );
+  });
+
+  test("tabSize from VSCode workspace", () => {
+    let output = extension.getFormattedText(
+      "select id, first_name from people",
+      <extension.WorkspaceConfiguration>{},
+      <extension.FormattingOptions>{ tabSize: 8 }
+    );
+    assert.equal(
+      output,
+      `\
+SELECT
+        id,
+        first_name
+FROM
+        people
+`
+    );
+  });
 });
